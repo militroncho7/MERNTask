ec0b84376555da492a8b2ae9514a965bf8b6d5b6
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-19 13:21:14 +0200

    finterfaz tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -23,6 +23,11 @@ const ListadoTareas = () => {
                 ))
                 }
             </ul>
+
+            <button
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar Proyecto &times;</button>
         </Fragment>
     );
 }
