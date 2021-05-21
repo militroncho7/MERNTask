2dbd55b27c63c095b540fb38904eff53eeb52a07
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-21 13:43:53 +0200

    eliminar proyectos

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -6,7 +6,7 @@ const ListadoTareas = () => {
 
     //Obtener State de proyectos
     const proyectosContext = useContext(proyectoContext);
-    const { proyecto } = proyectosContext;
+    const { proyecto, eliminarProyecto } = proyectosContext;
 
     //Si no hay proyecto seleccionado
     if(!proyecto) return <h2>Selecciona un proyecto</h2>;
@@ -19,7 +19,13 @@ const ListadoTareas = () => {
         { nombre: 'Elegir Colores', estado: false },
         { nombre: 'Elegir Plataformas de pago', estado: false },
         { nombre: 'Elegir Elegir Hosting', estado: true },
-    ]
+    ];
+
+    //Elimina un proyecto
+    const onClickEliminar = () => {
+        eliminarProyecto(proyectoActual.id);
+    }
+
     return (
         <Fragment>
             <h2>Proyecto: {proyectoActual.nombre}</h2>
@@ -38,6 +44,7 @@ const ListadoTareas = () => {
             <button
                 type="button"
                 className="btn btn-eliminar"
+                onClick={onClickEliminar}
             >Eliminar Proyecto &times;</button>
         </Fragment>
     );
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -8,7 +8,8 @@ import {
     OBTENER_PROYECTOS,
     AGREGAR_PROYECTO,
     VALIDAR_FORMULARIO,
-    PROYECTO_ACTUAL
+    PROYECTO_ACTUAL,
+    ELIMINAR_PROYECTO
     } from '../../types';
 
 
@@ -73,6 +74,14 @@ const ProyectoState = props => {
             type: PROYECTO_ACTUAL,
             payload: proyectoId
         })
+    };
+
+    //Elimina un proyecto
+    const eliminarProyecto = proyectoId => {
+        dispatch({
+            type: ELIMINAR_PROYECTO,
+            payload: proyectoId
+        })
     }
 
     return(
@@ -86,7 +95,8 @@ const ProyectoState = props => {
                 obtenerProyectos,
                 agregarProyecto,
                 mostrarError,
-                proyectoActual
+                proyectoActual,
+                eliminarProyecto
             }}
         >
             {props.children}
