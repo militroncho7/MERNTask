e8e46edce9957087c594b07a7d6c9c99c55d298e
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-24 10:08:11 +0200

    2 conttext: tareas!

---
+++ src/context/tareas/tareaState.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from 'react';
+import TareaContext from './tareaContext';
+import TareaReducer from './tareaReducer';
+
+const TareaState = props => {
+    
+    //definimos un state incial
+    const initialState = {
+        tareas: [],
+    }
+
+    //Crear dispatch y state
+    const [ state, dispatch ] = useReducer(TareaReducer, initialState);
+
+    return(
+        <TareaContext.Provider>
+            {props.children}
+        </TareaContext.Provider>
+    )
+};
+
+export default TareaState;
