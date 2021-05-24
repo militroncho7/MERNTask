b2face68137474d6c87ca89001b0b928b905052e
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-24 10:27:41 +0200

    funcio para obtener tareas

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -2,6 +2,10 @@ import React, { useReducer } from 'react';
 import TareaContext from './tareaContext';
 import TareaReducer from './tareaReducer';
 
+import { 
+    TAREAS_PROYECTO
+ } from '../../types';
+
 const TareaState = props => {
     
     //definimos un state incial
@@ -26,10 +30,21 @@ const TareaState = props => {
     //Crear dispatch y state
     const [ state, dispatch ] = useReducer(TareaReducer, initialState);
 
+    //Crear las funciones
+
+    //Obtener las tareas del proyecto
+    const obtenerTareas = proyectoId => {
+        dispatch({
+            type: TAREAS_PROYECTO,
+            payload: proyectoId
+        });
+    };
+
     return(
         <TareaContext.Provider
             value= {{
-                tareas: state.tareas
+                tareas: state.tareas,
+                obtenerTareas
             }}
         >
             {props.children}
