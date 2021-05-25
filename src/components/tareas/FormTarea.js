c91a1d42fd1684dabed730e21df5748311c023a3
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-05-25 11:53:40 +0200

    editar tareas

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
 import tareaContext from '../../context/tareas/tareaContext';
 
@@ -10,7 +10,18 @@ const FormTarea = () => {
 
     //Obtener la función del context de tarea
     const tareasContext = useContext(tareaContext);
-    const { errortarea, agregarTarea, validarTarea, obtenerTareas } = tareasContext;
+    const { tareaseleccionada, errortarea, agregarTarea, validarTarea, obtenerTareas } = tareasContext;
+
+    //useEffect que detecta si hya una tarea seleccionada
+    useEffect(() => {
+        if(tareaseleccionada !== null) {
+            guardarTarea(tareaseleccionada);
+        } else {
+            guardarTarea({
+                nombre: ''
+            })
+        }
+    }, [tareaseleccionada]);
 
     //state del formulario
     const [ tarea, guardarTarea ] = useState({
@@ -79,7 +90,7 @@ const FormTarea = () => {
                     <input
                         type="submit"
                         className="btn btn-primario btn-submit btn-block"
-                        value="Agregar Tarea"
+                        value={tareaseleccionada ? 'Editar tarea' : 'Agregar Tarea'}
                     />
                 </div>
             </form>
