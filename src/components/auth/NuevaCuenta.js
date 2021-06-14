f5c5c5ee0bd1094d43cd5e8ec4976e84ee44cab3
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-06-14 09:43:02 +0200

    validacion passwords

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -36,11 +36,20 @@ const NuevaCuenta = () => {
             password.trim() === '' ||
             confirmar.trim() === '') {
                 mostrarAlerta('Todos los campos son obligatorios', 'aletar-error');
+                return;
             };
 
         //Password minimo 6 caracteres
+        if(password.length < 6) {
+            mostrarAlerta('El password debe ser de al menos 6 caracteres', 'alerta-error');
+            return;
+        };
 
         //Los 2 passwords son iguales
+        if(password!== confirmar) {
+            mostrarAlerta('Los passwords no son iguales', 'alerta-error');
+            return;
+        };
 
         //pasarlo al action(reducer)
     };
